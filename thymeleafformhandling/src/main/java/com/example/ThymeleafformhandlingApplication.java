e38089521d05d4aad753eb1c64209e2665c9d0f4
DELL <DELL@DESKTOP-QA2UCA4>
2023-03-11 15:22:47 +0530

    Thymleaf,JPA,SpringBoot

---
+++ thymeleafformhandling/src/main/java/com/example/ThymeleafformhandlingApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@SpringBootApplication
+public class ThymeleafformhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafformhandlingApplication.class, args);
+	}
+// handler method to handle user registration page request
+}
+
